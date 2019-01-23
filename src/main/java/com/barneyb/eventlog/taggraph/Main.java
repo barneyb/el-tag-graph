ae8d08c6056cdd2552e1e23c626d1cef13367701
barneyb <bboisvert@gmail.com>
2019-01-23 12:40:03 -0800

    convert Main to run the GraphStream example

--- src/main/java/com/barneyb/eventlog/taggraph/Main.java
+++ src/main/java/com/barneyb/eventlog/taggraph/Main.java
@@ -1,9 +1,21 @@
 package com.barneyb.eventlog.taggraph;
 
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hello, world!");
+		Graph graph = new SingleGraph("Tutorial 1");
+
+		graph.addNode("A");
+		graph.addNode("B");
+		graph.addNode("C");
+		graph.addEdge("AB", "A", "B");
+		graph.addEdge("BC", "B", "C");
+		graph.addEdge("CA", "C", "A");
+
+		graph.display();
 	}
 
 }
